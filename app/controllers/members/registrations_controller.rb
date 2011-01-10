9088e0ae3c0483ae22616dc88d470e0005c7d44b
Jonathan Chiu <Jonathan@Jonathan-PC.(none)>
2011-01-10 16:58:23 +0800

    Added ActiveRecord Rollback to member registration

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -2,47 +2,45 @@
 class Members::RegistrationsController < Devise::RegistrationsController
 	
   def create
-   build_resource
+  	
+		build_resource
+	  
+		Member.transaction do
+			
+			if resource.save
+			  			
+				if resource.role == 'model'
+	    		unless resource.create_model
+			  		raise ActiveRecord::Rollback
+					end
+				elsif resource.role == 'photographer'
+	    	 	unless resource.create_photographer
+						raise ActiveRecord::Rollback
+					end
+	    	elsif resource.role == 'modeling_agent'
+	    		unless resource.create_modeling_agent
+						raise ActiveRecord::Rollback
+					end
+				end
 
-    if resource.valid?
-	
-	if params[:type] == 'model'
- 	    
-		resource.role = 'model'
-	    	resource.save
+      	if resource.active?
+        	set_flash_message :notice, :signed_up
+        	sign_in_and_redirect(resource_name, resource)
+					return
+      	else
+        	set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s
+        	expire_session_data_after_sign_in!
+        	redirect_to after_inactive_sign_up_path_for(resource)
+      	end
+    
+		 	end
+  
+		clean_up_passwords(resource)
+  	render_with_scope :new
+   	
+		end
 
-	    	resource.create_model
-	    
-	elsif params[:type] == 'photographer'
-	    
-		resource.role = 'photographer'
-	    	resource.save
-
-	    	resource.create_photographer
-	    
-	elsif params[:type] == 'modeling_agent'
-	    
-		resource.role = 'modeling_agent'
-	    	resource.save
-	    
-		resource.create_modeling_agent
-	
-	end
-
-      if resource.active?
-        set_flash_message :notice, :signed_up
-        sign_in_and_redirect(resource_name, resource)
-      else
-        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s
-        expire_session_data_after_sign_in!
-        redirect_to after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords(resource)
-      render_with_scope :new
-    end
   end
-
   def update
     if resource.update_with_password(params[resource_name])
       set_flash_message :notice, :updated
